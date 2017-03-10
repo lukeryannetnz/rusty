d9db43840c3da6756c9dd47c9a12f91a3b2f604b
Luke Ryan <github@lukeryan.net.nz>
2017-03-11 10:54:02 +1300

    Complete guessing game. Found rstfmt

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,24 +7,32 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("The secret number is: {}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Input your guess.");
+    loop {
+        println!("Input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Error reading line of text");
+        io::stdin().read_line(&mut guess).expect("Error reading line of text");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Error! Please type a number.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("higher..."),
-        Ordering::Greater => println!("lower..."),
-        Ordering::Equal => println!("YOU WIN!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("higher..."),
+            Ordering::Greater => println!("lower..."),
+            Ordering::Equal => {
+                println!("YOU WIN!");
+                break;
+            }
+        }
     }
 }
