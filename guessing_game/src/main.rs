672ef19fec32ea97f4358415df2e2f24ec8a46f7
Luke Ryan <github@lukeryan.net.nz>
2017-03-11 10:25:25 +1300

    Guessing game improving. Using matching and type conversion with parse.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is: {}", secret_number);
 
     println!("Input your guess.");
 
@@ -10,5 +17,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Error reading line of text");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Error! Please type a number.");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("higher..."),
+        Ordering::Greater => println!("lower..."),
+        Ordering::Equal => println!("YOU WIN!")
+    }
 }
