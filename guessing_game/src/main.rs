6eb12c84df979c314c4b49511931c169f07336dc
Luke Ryan <github@lukeryan.net.nz>
2017-03-11 09:54:27 +1300

    First step of guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Error reading line of text");
+
+    println!("You guessed: {}", guess);
 }
